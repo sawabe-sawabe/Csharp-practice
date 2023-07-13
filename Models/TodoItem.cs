3b6a82f831b3368e5e63b6f4a0aa013cfc206941
sawabe-sawabe <kapo0515.0515@gmail.com>
2023-07-14 05:48:30 +0900

    models

---
+++ Models/TodoItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set;}
+        public string Name { get;set; }
+        public string IsDone { get;set; }
+    }
+}
